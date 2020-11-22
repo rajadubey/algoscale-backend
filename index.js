ef33deb80068666d5463b4ff9481404351571851
rajadubey1997 <rajadubey1997@gmail.com>
2020-11-22 17:21:41 +0530

    final commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require('cors');
 app.use(express.json());
-
-const PORT = 4000;
+app.use(cors());
+const PORT = 5000;
 
 //connect to db
 mongoose
-  .connect("mongodb://localhost/alogoscale")
+  .connect("mongodb://localhost/algoscale")
   .then(() => console.log("Connected to Database"))
   .catch((err) => console.log("Error in MongoDB Connection" + err));
 
@@ -32,15 +33,20 @@ app.post("/submitForm", (request, response) => {
   const email = request.body.email;
   const message = request.body.message;
   const time = Date.now();
-
+  console.log(name+ ' is added - 1');  
   const newMessage = new Message({ name, email, message, time });
-  newMessage.save().then(() => response.sendStatus(200).json({message: "Success"})).catch((err)=>sendStatus(400).json({ error }));
+  newMessage.save().then(() => {
+      console.log(name+ ' is added - 2');  
+    response.sendStatus(200).json({message: "Success"})
+  }).catch((err)=>sendStatus(400).json({ error }));
+  console.log(name+ ' is added - 3');  
 });
 
 //returns all the messages count for every day between the given range of dates
 app.post("/forms", (request, response) => {
   const startDate = Number(request.body.startDate);
   const endDate = Number(request.body.endDate);
+  console.log(startDate, endDate);
   const oneDay = 86400000;
   Message.find()
     .then((data) => {
